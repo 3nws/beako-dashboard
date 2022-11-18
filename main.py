b72756636aff3b825de5585a8a640c30f4dd4230
Enes <eneskurbetoglu1@gmail.com>
2022-11-18 22:23:43 +0300

    handle client disconnection

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ from kivy.uix.boxlayout import BoxLayout
 
 from threading import Thread
 
-kivy.require('2.1.0')
+kivy.require("2.1.0")
 
 from kivy.config import Config
-Config.set('graphics', 'width', '1024')
-Config.set('graphics', 'height', '1024')
+
+Config.set("graphics", "width", "1024")
+Config.set("graphics", "height", "1024")
 
 
 host = "127.0.0.1"
@@ -46,21 +47,30 @@ class BeakoStats(BoxLayout):
             sys.exit()
 
         print("# Connecting to server, " + host + " (" + self.remote_ip + ")")
-        self.s.connect((self.remote_ip, port))
         while True:
-            reply = self.s.recv(4096)
-            self.servers = "Servers: " + str(json.loads(reply.decode()).get("servers"))
-            self.members = "Members: " + str(json.loads(reply.decode()).get("members"))
-            stats = json.loads(reply.decode()).get("stats")
-            self.stats.data = [    # type: ignore
-                {"name.text": k, "value": str(v)} for k, v in stats.items()
-            ]
-            namespaces = json.loads(reply.decode()).get("namespaces")
-            self.namespaces.data = [  # type: ignore
-                {"name.text": str(namespace[0]), "value": str(namespace[-1])}
-                for namespace in namespaces
-            ]
-            time.sleep(1)
+            try:
+                reply = self.s.recv(4096)
+                self.servers = "Servers: " + str(
+                    json.loads(reply.decode()).get("servers")
+                )
+                self.members = "Members: " + str(
+                    json.loads(reply.decode()).get("members")
+                )
+                stats = json.loads(reply.decode()).get("stats")
+                self.stats.data = [  # type: ignore
+                    {"name.text": k, "value": str(v)} for k, v in stats.items()
+                ]
+                namespaces = json.loads(reply.decode()).get("namespaces")
+                self.namespaces.data = [  # type: ignore
+                    {"name.text": str(namespace[0]), "value": str(namespace[-1])}
+                    for namespace in namespaces
+                ]
+                time.sleep(1)
+            except OSError:
+                try:
+                    self.s.connect((self.remote_ip, port))
+                except ConnectionRefusedError:
+                    pass
 
 
 class BeakoStatsApp(App):
@@ -68,8 +78,13 @@ class BeakoStatsApp(App):
         self.stats = BeakoStats()
         return self.stats
 
+    def on_request_close(self, *args):
+        print("here")
+        self.stats.s.close()
+        self.stats.socket_th.join()
+        return False
+
 
 if __name__ == "__main__":
     app = BeakoStatsApp()
     app.run()
-    app.stats.socket_th.join()
